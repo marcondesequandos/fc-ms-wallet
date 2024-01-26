29b16a035091d4ea087db07b02e0d6dd49c07126
felipe.carvalho.glo <felipe.carvalho@globalhitss.com.br>
2024-01-26 08:43:24 -0300

    criando projeto

---
+++ internal/entity/client_test.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatingNewClient(t *testing.T) {
+	client, err := NewClient("John Doe", "j@j.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "John Doe", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+func TestCreatingNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
