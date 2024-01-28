35bf2bd61c467decf03ea0aa3da9b061da4597e9
felipe.carvalho.glo <felipe.carvalho@globalhitss.com.br>
2024-01-27 21:28:27 -0300

    Adicionando entidade de account

--- internal/entity/client.go
+++ internal/entity/client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ID        string
 	Name      string
 	Email     string
+	Accounts  []*Account
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -37,5 +38,24 @@ func (c *Client) Validate() error {
 	if c.Email == "" {
 		return errors.New("email is required")
 	}
+	return nil
+}
+
+func (c *Client) Update(name string, email string) error {
+	c.Name = name
+	c.Email = email
+	c.UpdatedAt = time.Now()
+	err := c.Validate()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) AddAccount(account *Account) error {
+	if account.Client.ID != c.ID {
+		return errors.New("account does not belong to client")
+	}
+	c.Accounts = append(c.Accounts, account)
 	return nil
 }
--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -18,3 +18,25 @@ func TestCreatingNewClientWhenArgsAreInvalid(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, client)
 }
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	err := client.Update("John Doe Update", "j@j.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "John Doe Update", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	err := client.Update("", "j@j.com")
+	assert.Error(t, err, "name is required")
+
+}
+
+func TestAddAccountToClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j")
+	account := NewAccount(client)
+	err := client.AddAccount(account)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(client.Accounts))
+}
